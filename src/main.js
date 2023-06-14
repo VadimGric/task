b54ca878bfef829bd00f31645696045a3868ca5f
Vadim Gritskevich <vadim.gritskevich33@gmail.com>
2023-06-14 20:09:05 +0300

    change task

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+class Main extends React.Component {
+  render(){
+  return (
+    <div className="main">
+      <Header/>
+      <Body/>
+    </div>
+  );
+}
+}
+export default Main;
