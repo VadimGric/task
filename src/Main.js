c09db89236201510a6040ade02fed67fbc3d2a8e
VadimGric <83668950+VadimGric@users.noreply.github.com>
2023-06-26 13:17:44 +0300

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -2,7 +2,7 @@ import React from 'react';
 class Main extends React.Component {
   render(){
   return (
-    <div className="main">
+    <div className="Main">
       <Header/>
        <h2>ghjjj</h2>
       <Body/>
