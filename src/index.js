2f6d37c5c94573268e17c7830e27dbdee63a5ed1
VadimGric <83668950+VadimGric@users.noreply.github.com>
2023-06-26 13:13:42 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.sass';
-
+import Main from './Main.js'
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <main />
+    <Main />
   </React.StrictMode>
 );
 
