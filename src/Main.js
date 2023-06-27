29a8fac80fe6fb1a6c339ad753bc004813c1d94c
Vadim Gritskevich <vadim.gritskevich33@gmail.com>
2023-06-27 20:13:32 +0300

    hot fix

--- src/Main.js
+++ src/Main.js
@@ -3,9 +3,9 @@ class Main extends React.Component {
   render(){
   return (
     <div className="Main">
-      <Header/>
-       <h2>ghjjj</h2>
-      <Body/>
+      
+       
+      
     </div>
   );
 }
