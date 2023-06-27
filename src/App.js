fbaacb6f11b7a13b1736dbc1936a0f24d0ed00c2
Vadim Gritskevich <vadim.gritskevich33@gmail.com>
2023-06-27 20:47:41 +0300

    hot fix

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Admin, Resource, ListGuesser, EditGuesser, ShowGuesser } from "react-admin";
+import { dataProvider } from "./dataProvider";
+
+export const App = () => <Admin dataProvider={dataProvider}>
+
+
+
+    
+</Admin>;
